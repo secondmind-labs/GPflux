ca6f395e18ea963508af359421446d738b7cca0f
Marcin Tomczak <marcinbronislaw@gmail.com>
2018-10-31 13:54:04 +0000

    add smooth way of installing the package

--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@
 from setuptools import setup
 
 requirements = [
-    'numpy',
-    'tensorflow',
-    'scipy',
-    'sklearn'
-    # don't import gpflow here as we need develop, add back in when we depend on the released version
+    'numpy==1.15.1',
+    'tensorflow==1.12.0rc2',
+    'scipy==0.19.0',
+    'scikit-learn==0.20.0',
+    'pytest-cov==2.5.1',
+    'jupyter==1.0.0',
+    'sacred==0.7.4 ',
+    'pandas==0.23.4 '
 ]
 
 setup(name='gpflux',
       version="alpha",
       author="Prowler.io",
       author_email="vincent@prowler.io",
-      description=("GPFlux: Deep GP library"),
+      description="GPFlux: Deep GP library",
       keywords="Deep-Gaussian-processes",
       install_requires=requirements)
