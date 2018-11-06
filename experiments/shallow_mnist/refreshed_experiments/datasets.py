6d07955e73334cb53ecd7fc11a2ba82e0b629f0e
Marcin Tomczak <marcinbronislaw@gmail.com>
2018-11-06 12:01:48 +0000

    commit the upgraded entrypoint

--- experiments/shallow_mnist/refreshed_experiments/datasets.py
+++ experiments/shallow_mnist/refreshed_experiments/datasets.py
@@ -1,12 +1,24 @@
-from keras.datasets import cifar10, cifar100, mnist, fashion_mnist
+import numpy as np
 
 from experiments.shallow_mnist.refreshed_experiments.utils import rgb2gray
+
+
+class mnist:
+
+    @staticmethod
+    def load_data():
+        from keras.datasets import mnist
+        (train_features, train_targets), (test_features, test_targets) = mnist.load_data()
+        train_features = rgb2gray(train_features)
+        test_features = rgb2gray(test_features)
+        return (train_features, train_targets), (test_features, test_targets)
 
 
 class grey_cifar10:
 
     @staticmethod
     def load_data():
+        from keras.datasets import cifar10
         (train_features, train_targets), (test_features, test_targets) = cifar10.load_data()
         train_features = rgb2gray(train_features)
         test_features = rgb2gray(test_features)
@@ -17,7 +29,48 @@ class grey_cifar100:
 
     @staticmethod
     def load_data():
+        from keras.datasets import cifar100
         (train_features, train_targets), (test_features, test_targets) = cifar100.load_data()
         train_features = rgb2gray(train_features)
         test_features = rgb2gray(test_features)
         return (train_features, train_targets), (test_features, test_targets)
+
+
+def _get_dataset_fraction(dataset, fraction):
+    (train_features, train_targets), (test_features, test_targets) = dataset.load_data()
+    seed = np.random.get_state()
+    # fix the seed for numpy
+    np.random.seed(0)
+    indices = np.random.permutation(range(train_features.shape[0]))[
+              :int(train_features.shape[0] * fraction)]
+    train_features, train_targets, test_features, test_targets \
+        = train_features[indices], train_targets[indices], test_features[indices], test_targets[
+        indices]
+    train_features = rgb2gray(train_features)
+    test_features = rgb2gray(test_features)
+    np.random.seed(seed)
+    return (train_features, train_targets), (test_features, test_targets)
+
+
+class mnist_10percent:
+
+    @staticmethod
+    def load_data():
+        from keras.datasets import mnist
+        return _get_dataset_fraction(mnist, 0.1)
+
+
+class mnist_25percent:
+
+    @staticmethod
+    def load_data():
+        from keras.datasets import mnist
+        return _get_dataset_fraction(mnist, 0.25)
+
+
+class mnist_50percent:
+
+    @staticmethod
+    def load_data():
+        from keras.datasets import mnist
+        return _get_dataset_fraction(mnist, 0.5)
--- experiments/shallow_mnist/refreshed_experiments/main.py
+++ experiments/shallow_mnist/refreshed_experiments/main.py
@@ -1,6 +1,8 @@
+import argparse
 from keras.datasets import mnist, fashion_mnist
 
 from experiments.shallow_mnist.refreshed_experiments.conv_gp.creators import convgp_creator
+from experiments.shallow_mnist.refreshed_experiments.datasets import grey_cifar10
 from experiments.shallow_mnist.refreshed_experiments.nn.creators import mnist_cnn_creator, \
     cifar_cnn_creator
 from experiments.shallow_mnist.refreshed_experiments.nn.configs import MNISTCNNConfiguration, \
@@ -8,7 +10,6 @@ from experiments.shallow_mnist.refreshed_experiments.nn.configs import MNISTCNNC
 from experiments.shallow_mnist.refreshed_experiments.conv_gp.configs import ConvGPConfig
 from experiments.shallow_mnist.refreshed_experiments.experiment_infrastructure import Experiment, \
     ExperimentSuite, KerasNNTrainer, GPTrainer
-from experiments.shallow_mnist.refreshed_experiments.datasets import grey_cifar10
 from experiments.shallow_mnist.refreshed_experiments.data_infrastructure import DummyPreprocessor, \
     ImageClassificationDataset
 
@@ -21,23 +22,94 @@ Things to consider:
 - ability to run a model vs different optimisation setups, i.e. don't change the inference scheme, 
 but change the optimisation procedure
 - ability to run within docker images so we can run the experiments anywhere if needed
-- ability to use autocompletion when implementing stuff - this can be achieved by delating with
+- ability to use autocompletion when implementing stuff - this can be achieved by dealing with
 python objects
 
 The following setup is merely an example.
 """
 
 
+def _get_dataset(dataset_name):
+    from experiments.shallow_mnist.refreshed_experiments.datasets \
+        import \
+        mnist, \
+        grey_cifar10, \
+        grey_cifar100, \
+        mnist_10percent, \
+        mnist_25percent, \
+        mnist_50percent
+    datasets = mnist, \
+               grey_cifar10, \
+               grey_cifar100, \
+               mnist_10percent, \
+               mnist_25percent, \
+               mnist_50percent
+    dataset_dict = {d.__name__: d for d in datasets}
+    try:
+        return dataset_dict[dataset_name]
+    except KeyError:
+        raise KeyError('Dataset {} not found. Available are: {}'.format(dataset_name, ' '.join(
+            dataset_dict.keys())))
+
+
+def _get_model_creator(model_creator_name):
+    from experiments.shallow_mnist.refreshed_experiments.nn.creators \
+        import \
+        cifar_cnn_creator, \
+        mnist_cnn_creator, \
+        mnist_fashion_cnn_creator
+    model_creators = cifar_cnn_creator, \
+                     mnist_cnn_creator, \
+                     mnist_fashion_cnn_creator
+    model_creator_dict = {mc.__name__: mc for mc in model_creators}
+    try:
+        return model_creator_dict[model_creator_name]
+    except KeyError:
+        raise KeyError('Model creator {} '
+                       'not found. Available are: {}'.format(model_creator_name,
+                                                             ' '.join(
+                                                                 model_creator_dict.keys())))
+
+
+def _get_model_config(config_name):
+    from experiments.shallow_mnist.refreshed_experiments.nn.configs \
+        import \
+        MNISTCNNConfiguration, \
+        CifarCNNConfiguration
+    configs = MNISTCNNConfiguration, \
+              CifarCNNConfiguration
+    configs_dict = {c.__name__: c for c in configs}
+    try:
+        return configs_dict[config_name]
+    except KeyError:
+        raise KeyError('Config {} not found. Available are: {}'.format(config_name,
+                                                                       ' '.join(
+                                                                           configs_dict.keys())))
+
+
 def main():
+    parser = argparse.ArgumentParser(description='Entrypoint for running the experiments.')
+    parser.add_argument('--model_creator', help='the model creator', type=str)
+    parser.add_argument('--model_configuration', help='the model configuration', type=str)
+    parser.add_argument('--dataset', help='dataset', type=str)
+    parser.add_argument('--trainer', help='used_trainer', type=str)
+
+    args = parser.parse_args()
+    dataset = _get_dataset(args.dataset)
+    model_creator = _get_model_creator(args.model_creator)
+    config = _get_model_config(args.config)
+
     gp_experiment_mnist = Experiment('convgp_experiment',
                                      trainer=GPTrainer(convgp_creator, config=ConvGPConfig),
-                                     dataset=ImageClassificationDataset.from_keras_format(mnist),
+                                     dataset=ImageClassificationDataset.from_keras_format(
+                                         mnist),
                                      dataset_preprocessor=DummyPreprocessor)
 
     cnn_experiment_mnist = Experiment('cnn_experiment',
                                       trainer=KerasNNTrainer(mnist_cnn_creator,
                                                              config=MNISTCNNConfiguration),
-                                      dataset=ImageClassificationDataset.from_keras_format(mnist),
+                                      dataset=ImageClassificationDataset.from_keras_format(
+                                          mnist),
                                       dataset_preprocessor=DummyPreprocessor)
 
     cnn_experiment_fashion_mnist = Experiment('cnn_experiment',
@@ -54,9 +126,7 @@ def main():
                                             grey_cifar10),
                                         dataset_preprocessor=DummyPreprocessor)
 
-    experiment_suite = ExperimentSuite(experiment_list=[cnn_experiment_mnist,
-                                                        cnn_experiment_fashion_mnist,
-                                                        cnn_experiment_cifar10])
+    experiment_suite = ExperimentSuite(experiment_list=[cnn_experiment_mnist])
     experiment_suite.run()
 
 
