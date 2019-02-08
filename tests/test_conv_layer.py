fb389b43589894346d1e0d68d2b2be9acd9f4b45
Marcin Tomczak <marcinbronislaw@gmail.com>
2019-02-08 11:53:32 +0000

    add fix and test catching bug

---
+++ tests/test_conv_layer.py
@@ -0,0 +1,17 @@
+import gpflow
+
+from gpflux.layers import ConvLayer
+import tensorflow as tf
+
+
+def test_conv_layer_uses_same_graph():
+    # check if defer build applies to all tensors/variables created by the ConvLayer
+    graph = tf.get_default_graph()
+    with gpflow.defer_build():
+        ConvLayer(input_shape=[5, 5],
+                  output_shape=[4, 4],
+                  number_inducing=5,
+                  patch_shape=[2, 2],
+                  with_indexing=True)
+
+    assert len(graph.as_graph_def().node) == 0
