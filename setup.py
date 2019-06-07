52752c5626219c41e26c04ee8b90db8bf494c6d6
ST John <st@prowler.io>
2019-06-07 16:01:41 +0100

    bump versions

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 from setuptools import setup
 
 requirements = [
-    'numpy==1.15.4',
+    'numpy==1.16.3',
     'tensorflow==1.13.1',
-    'scipy==1.1.0',
+    'scipy==1.2.1',
     'scikit-learn==0.20.2',
-    'matplotlib==2.2.2',
+    'matplotlib==3.0.3',
     'flake8==3.7.7',
     'mypy==0.670',
     'tqdm==4.28.1',
