49affc2b5aeedeabd94cc94beb65b0efec2e3e54
st-- <st--@users.noreply.github.com>
2020-01-06 17:50:59 +0000

    remove pinning from setup.py (#39)
    Only have version lower bounds were required, otherwise leave package versions free for user to choose.

--- setup.py
+++ setup.py
@@ -4,23 +4,13 @@
 from setuptools import setup
 
 requirements = [
-    'numpy==1.17.4',
-    'tensorflow==2.0.0',
-    'tensorflow-probability==0.8.0',
-    'scipy==1.3.2',
-    'scikit-learn==0.20.2',
-    'matplotlib==3.1.1',
-    'flake8==3.7.7',
-    'mypy==0.670',
-    'tqdm==4.38.0',
-    'gpflow==2.0.0rc1',
-    # for tests:
-    'pytest-cov==2.7.1',
-    'nbformat==4.4.0',
-    'nbconvert==5.4.1',
-    'jupyter_client==5.2.4',
-    'ipykernel==5.1.0',
-    'tornado==5.1.1',  # tornado 6 is broken
+    'numpy',
+    'scipy',
+    'scikit-learn',
+    'matplotlib',
+    'tensorflow>=2.0.0',
+    'tensorflow-probability>=0.8.0',
+    'gpflow>=2.0.0rc1',
 ]
 
 setup(name='gpflux',
