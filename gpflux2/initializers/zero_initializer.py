d2f1d587a28d240c68f0a839d0627039ff71b593
ST John <st@prowler.io>
2020-01-18 20:49:51 +0000

    introduce VariationalInitializer base class that handles the init_variational_params for both ZeroOne and FeedForward

--- gpflux2/initializers/zero_initializer.py
+++ gpflux2/initializers/zero_initializer.py
@@ -6,30 +6,20 @@ from typing import Optional
 import numpy as np
 import tensorflow as tf
 
-from gpflux2.initializers import Initializer
+from gpflux2.initializers import VariationalInitializer
 
 
-class ZeroOneInitializer(Initializer):
+class ZeroOneInitializer(VariationalInitializer):
     """
     Base object that initialises variational parameters to zero mean, identity
     covariance, and inducing points to a given value. This is useful in
     testing, as starting KL divergences are exactly 0.
     """
 
-    def __init__(self, Z: Optional[np.ndarray] = None):
-        super().__init__(init_at_predict=False)
+    def __init__(self, Z: Optional[np.ndarray] = None,
+                 q_sqrt_diagonal: float = 1e-2):
+        super().__init__(init_at_predict=False, q_sqrt_diagonal=q_sqrt_diagonal)
         self.Z = Z
-
-    def init_variational_params(self, q_mu, q_sqrt) -> None:
-        """Initialise the variational parameter to a zero mean and """
-        num_inducing_vars, num_output_dims = q_mu.shape
-        q_mu_value = np.zeros((num_inducing_vars, num_output_dims))
-        q_sqrt_value = (
-            np.tile(np.eye(num_inducing_vars), (num_output_dims, 1, 1))
-        )
-
-        q_mu.assign(q_mu_value)
-        q_sqrt.assign(q_sqrt_value)
 
     def init_inducing_variable(self, inducing_variable, inputs=None) -> None:
         if hasattr(inducing_variable, "inducing_variable_list"):
