09304d5f3573cd43938b693deb42ad2692c9201f
ST John <st@prowler.io>
2020-01-20 14:23:12 +0000

    extent comments in test_bayesbench

--- tests_gpflux2/test_bayesbench.py
+++ tests_gpflux2/test_bayesbench.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import importlib.util
 import pytest
+import gpflow
+import tensorflow as tf
 
 def import_module_from_path(path, module_name):
     """Import a module based on the path as opposed to the module name"""
@@ -24,12 +26,30 @@ def get_snelson_X_and_Y():
     data = np.load(os.path.join(this_dir(), 'snelson1d.npz'))
     return data['X'], data['Y']
 
+def compute_gpr_lml(X, Y):
+    model = gpflow.models.GPR((X, Y), gpflow.kernels.SquaredExponential(), noise_variance=0.1)
+    neg_log_marginal_likelihood = tf.function(model.neg_log_marginal_likelihood)
+    gpflow.optimizers.Scipy().minimize(neg_log_marginal_likelihood,
+                                       model.trainable_variables, jit=True)
+    return - neg_log_marginal_likelihood()
+
 def test_bayesbench_deepgp_snelson(bayesbench_deepgp):
     X, Y = get_snelson_X_and_Y()
+
+    # log marginal likelihood of GPR with SquaredExponential kernel
+    gpr_lml = compute_gpr_lml(X, Y)
+    expected_elbo = - 55.9003
+    assert np.allclose(gpr_lml, expected_elbo)
+
     bench = bayesbench_deepgp.BayesBench_DeepGP()
     bench.Config.MAXITER = 3000
     bench.fit(X, Y)
     elbo = bench.log_pdf(X, Y)
-    expected_elbo = - 55.9003  # from log marginal likelihood of GPR with SquaredExponential kernel
+
     assert np.allclose(elbo, expected_elbo, rtol=0.02)  # 3000 steps not quite converged yet
     # with more optimisation steps could reduce rtol to 0.001
+
+    # TODO: significant difference between runs - sometimes it hits ~ -56 after ~2000 steps
+    # sometimes gets stuck in local optimum a few nats worse. Can we identify what these
+    # optima are? Can we identify this after a few steps? Should we do random restarts more
+    # generally in DGPs ?
