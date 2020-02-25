491ccd01b7bdc42a67d622b427988f54b4616d54
ST John <st@prowler.io>
2020-02-25 09:42:11 +0000

    refactor verify_compatibility tests

--- tests/gpflux/utils/test_runtime_checks.py
+++ tests/gpflux/utils/test_runtime_checks.py
@@ -3,7 +3,7 @@
 # Proprietary and confidential
 import pytest
 
-from gpflow.kernels import RBF
+from gpflow.kernels import Matern52
 from gpflow.mean_functions import Zero
 from gpflow.inducing_variables import InducingPoints
 
@@ -12,60 +12,58 @@ from gpflux.helpers import construct_basic_kernel, construct_basic_inducing_vari
 from gpflux.exceptions import ShapeIncompatibilityError
 
 
-def make_kernels_and_inducing_variables():
-    input_dim = 40  # has no effect on compatibility in these tests
-
-    num_latents = [(3, 3), (5, 10)]  # (kernel_latents, iv_latents)
-    test_combos = []
-    for num_latent_k, num_latent_iv in num_latents:
-
-        kernels = [
-            construct_basic_kernel([RBF() for _ in range(num_latent_k)]),
-            construct_basic_kernel(
-                RBF(), output_dim=num_latent_k, share_hyperparams=False
-            ),
-            construct_basic_kernel(
-                RBF(), output_dim=num_latent_k, share_hyperparams=True
-            ),
-        ]
-
-        inducing_variables = [
-            construct_basic_inducing_variables(
-                [35 for _ in range(num_latent_iv)], input_dim=40
-            ),
-            construct_basic_inducing_variables(
-                35, input_dim=40, output_dim=num_latent_iv
-            ),
-        ]
-
-        are_compatible = num_latent_k == num_latent_iv
-        test_combos.extend(
-            [(k, iv, are_compatible) for k in kernels for iv in inducing_variables]
-        )
-    print(test_combos)
-    return test_combos
+# has no effect on compatibility in these tests
+input_dim = 7
+num_inducing = 35
 
 
-@pytest.mark.parametrize(
-    "kernel,inducing_variable,are_compatible", make_kernels_and_inducing_variables()
-)
-def test_verify_compatibility_num_latents(kernel, inducing_variable, are_compatible):
+def make_kernels(num_latent_k):
+    return [
+        construct_basic_kernel([Matern52() for _ in range(num_latent_k)]),
+        construct_basic_kernel(
+            Matern52(), output_dim=num_latent_k, share_hyperparams=False
+        ),
+        construct_basic_kernel(
+            Matern52(), output_dim=num_latent_k, share_hyperparams=True
+        ),
+    ]
+
+
+def make_inducing_variables(num_latent_iv):
+    return [
+        construct_basic_inducing_variables(
+            num_inducing=[num_inducing for _ in range(num_latent_iv)],
+            input_dim=input_dim,
+        ),
+        construct_basic_inducing_variables(
+            num_inducing=num_inducing, input_dim=input_dim, output_dim=num_latent_iv
+        ),
+    ]
+
+
+@pytest.mark.parametrize("kernel", make_kernels(3))
+@pytest.mark.parametrize("inducing_variable", make_inducing_variables(3))
+def test_verify_compatibility_num_latents_compatible(kernel, inducing_variable):
     mean_function = Zero()
-    if are_compatible:
-        _, num_latents = verify_compatibility(kernel, mean_function, inducing_variable)
-        assert num_latents == kernel.num_latents
-    else:
-        with pytest.raises(ShapeIncompatibilityError):
-            verify_compatibility(kernel, mean_function, inducing_variable)
+    _, num_latents = verify_compatibility(kernel, mean_function, inducing_variable)
+    assert num_latents == kernel.num_latents
+
+
+@pytest.mark.parametrize("kernel", make_kernels(5))
+@pytest.mark.parametrize("inducing_variable", make_inducing_variables(10))
+def test_verify_compatibility_num_latents_incompatible(kernel, inducing_variable):
+    mean_function = Zero()
+    with pytest.raises(ShapeIncompatibilityError):
+        verify_compatibility(kernel, mean_function, inducing_variable)
 
 
 def test_verify_compatibility_type_errors():
     valid_inducing_variable = construct_basic_inducing_variables([35], input_dim=40)
-    valid_kernel = construct_basic_kernel([RBF()])
+    valid_kernel = construct_basic_kernel([Matern52()])
     valid_mean_function = Zero()  # all gpflow mean functions are currently valid
 
     with pytest.raises(TypeError):  # gpflow kernels must be MultioutputKernels
-        verify_compatibility(RBF(), valid_mean_function, valid_inducing_variable)
+        verify_compatibility(Matern52(), valid_mean_function, valid_inducing_variable)
 
     Z = valid_inducing_variable.inducing_variable_list[0].Z
     inducing_variable = InducingPoints(Z)
