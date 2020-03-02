9a45a67bbea7da84caed4e04d4e8c786855d507b
st-- <st--@users.noreply.github.com>
2020-03-02 18:59:08 +0000

    Refactor initializers and add Kmeans initializer (#82)
    Resolves #81:
    
    - makes FeedForwardInitializer pad with randn if minibatch size (length of `inputs`) is smaller than the number of inducing points
    - breaks out initialization of q(u) into VariationalInitializer classes (in gpflux.initializers.variational); Initializer subclasses now take qu_initializer argument and have to handle inducing variable initialisation
    - some renames to make things cleaner, e.g. ZeroOneInitializer -> ZZeroOneInitializer
    - adds a KmeansInitializer
    - adds tests for initializers

--- gpflux/initializers/initializer.py
+++ gpflux/initializers/initializer.py
@@ -3,44 +3,54 @@
 # Proprietary and confidential
 
 from abc import ABC, abstractmethod
-import numpy as np
+from typing import Optional
+
+from gpflow.inducing_variables import InducingPoints
+
+from gpflux.initializers.variational import (
+    VariationalInitializer,
+    MeanFieldVariationalInitializer,
+)
 
 
 class Initializer(ABC):
     """Base object that initialises variational parameters and inducing points"""
 
-    def __init__(self, init_at_predict: bool):
+    def __init__(
+        self,
+        init_at_predict: bool,
+        qu_initializer: Optional[VariationalInitializer] = None,
+    ):
         self.init_at_predict = init_at_predict
-
-    @abstractmethod
-    def init_variational_params(self, q_mu, q_sqrt) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def init_inducing_variable(self, inducing_variable, inputs) -> None:
-        raise NotImplementedError
-
-
-class VariationalInitializer(Initializer):
-    """
-    Base object that initialises variational parameters to zero-mean, diagonal
-    covariance pointlike distributions.
-    """
-
-    def __init__(self, init_at_predict: bool, q_sqrt_diagonal: float):
-        super().__init__(init_at_predict=init_at_predict)
-        self.q_sqrt_diagonal = q_sqrt_diagonal
+        if qu_initializer is None:
+            qu_initializer = MeanFieldVariationalInitializer()
+        self.qu_initializer = qu_initializer
 
     def init_variational_params(self, q_mu, q_sqrt) -> None:
-        """
-        Initialize the variational parameter to a zero mean and diagonal covariance
-        """
-        num_inducing_vars, num_output_dims = q_mu.shape
-        q_mu_value = np.zeros((num_inducing_vars, num_output_dims))
-        q_sqrt_value = (
-            np.tile(np.eye(num_inducing_vars), (num_output_dims, 1, 1))
-            * self.q_sqrt_diagonal
-        )
+        self.qu_initializer.init_variational_params(q_mu, q_sqrt)
 
-        q_mu.assign(q_mu_value)
-        q_sqrt.assign(q_sqrt_value)
+    def init_inducing_variable(self, inducing_variable, inputs=None) -> None:
+        # HACK to deal with multioutput inducing variables
+        if hasattr(inducing_variable, "inducing_variable_list"):
+            inducing_variable_list = inducing_variable.inducing_variable_list
+        elif hasattr(inducing_variable, "inducing_variable_shared"):
+            inducing_variable_list = [inducing_variable.inducing_variable_shared]
+        else:
+            raise AttributeError("Could not find inducing variable attribute")
+
+        for inducing_var in inducing_variable_list:
+            if self.init_at_predict:
+                self.init_single_inducing_variable(inducing_var, inputs=inputs)
+            else:
+                self.init_single_inducing_variable(inducing_var)
+
+    @abstractmethod
+    def init_single_inducing_variable(
+        self, inducing_variable: InducingPoints, inputs=None
+    ) -> None:
+        """
+        Initializes the inducing variable (here assumed to be InducingPoints)
+        for a single GP.
+        Should only use `inputs` when self.init_at_predict is True.
+        """
+        raise NotImplementedError
