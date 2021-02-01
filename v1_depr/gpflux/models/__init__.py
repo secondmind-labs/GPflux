bd256f0d6802877959a541716865e893b8b01c6a
John Mcleod <43960404+johnamcleod@users.noreply.github.com>
2021-02-01 13:55:45 +0000

    Delete the `v1_depr` package (#159)

--- v1_depr/gpflux/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) PROWLER.io 2018 - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-
-from .deep_gp import DeepGP
--- v1_depr/gpflux/types.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) PROWLER.io 2018 - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-
-
-# Ideally this is Union[tf.Variable, tf.Tensor, np.ndarray]
-# but that doesn't play nicely with Multiple Dispatch
-TensorLike = object
