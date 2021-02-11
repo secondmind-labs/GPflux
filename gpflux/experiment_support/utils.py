feb529f5c956104423f998a9d299cb416e441f89
st-- <st--@users.noreply.github.com>
2021-02-11 09:51:30 +0000

    add type annotations (#172)
    * add type annotations
    * explicit isort src_paths

--- gpflux/experiment_support/utils.py
+++ gpflux/experiment_support/utils.py
@@ -5,18 +5,18 @@
 import os
 
 
-def is_continuous_integration():
+def is_continuous_integration() -> bool:
     ci = os.environ.get("CI", "").lower()
     return (ci == "true") or (ci == "1")
 
 
-def notebook_niter(n, test_n=2):
+def notebook_niter(n: int, test_n: int = 2) -> int:
     return test_n if is_continuous_integration() else n
 
 
-def notebook_range(n, test_n=2):
+def notebook_range(n: int, test_n: int = 2) -> range:
     return range(notebook_niter(n, test_n))
 
 
-def notebook_list(lst, test_n=2):
+def notebook_list(lst: list, test_n: int = 2) -> list:
     return lst[:test_n] if is_continuous_integration() else lst
