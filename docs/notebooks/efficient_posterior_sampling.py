28aca6efa52d7ab3d89e48a0f4b7c74addf24b38
Felix Leibfried <felix.leibfried@gmail.com>
2021-03-11 13:21:17 +0000

    Update docs/notebooks/efficient_posterior_sampling.py
    Co-authored-by: akhayes <44195080+akhayes@users.noreply.github.com>

--- docs/notebooks/efficient_posterior_sampling.py
+++ docs/notebooks/efficient_posterior_sampling.py
@@ -186,7 +186,7 @@ def log10_Wasserstein_distance(
     :param covariance: The analytic covariance, with a shape of [N* x N*].
     :param approximate_mean: The approximate mean, with a shape of [N*].
     :param approximate_covariance: The approximate covariance, with a shape of [N* x N*].
-    :param jitter: jitter value for numerical robustness
+    :param jitter: The jitter value for numerical robustness.
     :return: a scalar log distance value
     """
     squared_mean_distance = tf.norm(mean - approximate_mean) ** 2
