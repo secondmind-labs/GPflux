1ed44bc60ab0b4c18dcf88e89cec9a0dfc7ce207
Vincent Dutordoir <dutordoirv@gmail.com>
2021-04-26 11:08:57 +0200

    Release to PyPI (#21)

--- setup.py
+++ setup.py
@@ -11,14 +11,35 @@ requirements = [
     "tensorflow>=2.4.0",
 ]
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+with open("VERSION", "r") as file:
+    version = file.read().strip()
+
 setup(
     name="gpflux",
-    version="0.1",
+    version=version,
     author="Secondmind Labs",
     author_email="gpflux@secondmind.ai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description="GPflux: Deep GP library",
+    license="Apache License 2.0",
     keywords="Deep-Gaussian-processes",
     install_requires=requirements,
     packages=find_namespace_packages(include=["gpflux*"]),
     package_data={"gpflux": ["py.typed"]},
+    project_urls={
+        "Source on GitHub": "https://github.com/secondmind-labs/GPflux",
+        "Documentation": "https://secondmind-labs.github.io/GPflux/",
+    },
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
 )
