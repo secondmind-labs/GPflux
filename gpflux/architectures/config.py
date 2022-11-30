1060ff25d6b86f7a0862775edac9cd756ec61dd2
Alessandro Vullo <alessandro.vullo@prowler.io>
2022-11-30 17:05:33 +0000

    - Use configurations with models and likelihoods - Switch to factories for constant input dim architecture in the normal and orthogonal case.

---
+++ gpflux/architectures/config.py
@@ -0,0 +1,127 @@
+#
+# Copyright (c) 2022 The GPflux Contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""This module declares configurations for building various types of architectures"""
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Type
+
+import tensorflow_probability as tfp
+
+import gpflow.likelihoods
+from gpflow.kernels import Stationary
+
+
+@dataclass
+class LikelihoodConfig(ABC):
+    """Config for the model likelihood"""
+
+    @abstractmethod
+    def create(self) -> gpflow.likelihoods.Likelihood:
+        """Create a likelihood instance with the parameters of the config"""
+
+
+@dataclass
+class GaussianLikelihoodConfig(LikelihoodConfig):
+    """Config for a :class:`~gpflow.likelihoods.Gaussian` likelihood"""
+
+    noise_variance: float
+    """The variance of the likelihood"""
+
+    def create(self) -> gpflow.likelihoods.Gaussian:
+        return gpflow.likelihoods.Gaussian(variance=self.noise_variance)
+
+
+@dataclass
+class StudenttLikelihoodConfig(LikelihoodConfig):
+    """Config for a :class:`~gpflow.likelihoods.Studentt` likelihood"""
+
+    df: float
+    """The number of degrees of freedom"""
+
+    scale: float
+    """The scale parameter"""
+
+    def create(self) -> gpflow.likelihoods.StudentT:
+        return gpflow.likelihoods.StudentT(df=self.df, scale=self.scale)
+
+
+@dataclass
+class HeteroSkedasticLikelihoodConfig(LikelihoodConfig):
+    """Configuration for a :class:`~gpflow.likelihoods.HeteroskedasticTFPConditional`"""
+
+    distribution_class: Type[tfp.distributions.Distribution] = tfp.distributions.Normal
+    """The distribution class"""
+
+    def create(self) -> gpflow.likelihoods.HeteroskedasticTFPConditional:
+        return gpflow.likelihoods.HeteroskedasticTFPConditional(
+            distribution_class=self.distribution_class
+        )
+
+
+@dataclass
+class HyperParametersConfig:
+    """Configuration of the hyperparameters of a model"""
+
+    num_layers: int
+    """The number of GP layers in the model, excluded the likelihood one"""
+
+    kernel: Type[Stationary]
+    """The (stationary) kernel to use in the layers"""
+
+    likelihood: LikelihoodConfig
+    """Configuration for the model likelihood"""
+
+    inner_layer_qsqrt_factor: float
+    """
+    A multiplicative factor used to rescale the hidden layers'
+    :attr:`~gpflux.layers.GPLayer.q_sqrt`. Typically this value is chosen to be small
+    (e.g., 1e-5) to reduce noise at the start of training.
+    """
+
+    whiten: bool
+    """
+    Determines the parameterisation of the inducing variables.
+    If `True`, :math:``p(u) = N(0, I)``, otherwise :math:``p(u) = N(0, K_{uu})``.
+    .. seealso:: :attr:`gpflux.layers.GPLayer.whiten`
+    """
+
+
+@dataclass
+class ModelHyperParametersConfig(HyperParametersConfig):
+    """The configuration used to build a DeepGP model"""
+
+    num_inducing: int
+    """
+    The number of inducing variables, *M*. The Deep GP uses the same number
+    of inducing variables in each layer.
+    """
+
+
+@dataclass
+class OrthogonalModelHyperparametersConfig(HyperParametersConfig):
+    """The configuration used to build an OrthDeepGP model"""
+
+    num_inducing_u: int
+    """
+    The number of inducing variables, *M*. The Deep GP uses the same number
+    of inducing variables in each layer.
+    """
+
+    num_inducing_v: int
+    """
+    The number of inducing variables, *M*. The Deep GP uses the same number
+    of inducing variables in each layer.
+    """
---
+++ gpflux/architectures/factory.py
@@ -0,0 +1,229 @@
+#
+# Copyright (c) 2022 The GPflux Contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""This module defines factories for various Deep GP architectures"""
+from functools import singledispatch
+from typing import Type, cast
+
+import numpy as np
+import tensorflow as tf
+from scipy.cluster.vq import kmeans2
+
+from gpflow import default_float
+from gpflow.kernels import Stationary
+from gpflow.mean_functions import Zero
+
+from gpflux.architectures.config import (
+    HyperParametersConfig,
+    ModelHyperParametersConfig,
+    OrthogonalModelHyperparametersConfig,
+)
+from gpflux.helpers import (
+    construct_basic_inducing_variables,
+    construct_basic_kernel,
+    construct_mean_function,
+)
+from gpflux.layers.gp_layer import GPLayer, OrthGPLayer
+from gpflux.layers.likelihood_layer import LikelihoodLayer
+from gpflux.models import DeepGP, OrthDeepGP
+
+
+def build_kernel(input_dim: int, is_last_layer: bool, kernel: Type[Stationary]) -> Stationary:
+    """
+    Return a :class:`gpflow.kernels.Stationary` kernel with ARD lengthscales set to
+    1.0 and a small kernel variance of 1e-6 if the kernel is part of a hidden layer;
+    otherwise, the kernel variance is set to 1.0.
+
+    :param input_dim: The input dimensionality of the layer.
+    :param is_last_layer: Whether the kernel is part of the last layer in the Deep GP.
+    :param kernel: the :class:`~gpflow.kernels.Stationary` type of the kernel
+    """
+    assert input_dim > 0, 'Cannot have non positive input dimension'
+
+    variance = 1e-6 if not is_last_layer else 1.0
+    lengthscales = [1.0] * input_dim
+
+    return kernel(lengthscales=lengthscales, variance=variance)
+
+
+@singledispatch
+def build_constant_input_dim_architecture(
+    model_config: HyperParametersConfig, X: np.ndarray
+) -> DeepGP:
+    """
+    Build a Deep GP consisting of a number of :class:`GPLayer`\ s.
+    All the hidden layers have the same input dimension as the data, that is, ``X.shape[1]``.
+
+    The architecture is largely based on :cite:t:`salimbeni2017doubly`, with
+    the most notable difference being that we keep the hidden dimension equal
+    to the input dimensionality of the data.
+
+    .. note::
+        This architecture might be slow for high-dimensional data.
+
+    :param model_config: The configuration for (hyper)parameters.
+    :param X: The training input data, used to retrieve the number of datapoints and
+        the input dimension and to initialise the inducing point locations using k-means. A
+        tensor of rank two with the dimensions ``[num_data, input_dim]``.
+
+    :return: an instance of a DeepGP model
+    :raises ValueError: If the config type is not registered.
+    """
+    raise ValueError(
+        f"Don't know how to create model from config of type: {type(HyperParametersConfig)}"
+    )
+
+
+@build_constant_input_dim_architecture.register
+def build_constant_input_dim_deep_gp(
+    model_config: ModelHyperParametersConfig, X: np.ndarray
+) -> DeepGP:
+    if X.dtype != default_float():
+        raise ValueError(
+            f"X needs to have dtype according to gpflow.default_float() = {default_float()} "
+            f"however got X with {X.dtype} dtype."
+        )
+
+    num_data, input_dim = X.shape
+    X_running = X
+
+    gp_layers = []
+    centroids, _ = kmeans2(X, k=model_config.num_inducing, minit="points")
+
+    num_layers = model_config.num_layers
+    for i_layer in range(num_layers):
+        is_last_layer = i_layer == num_layers - 1
+        D_in = input_dim
+        D_out = 1 if is_last_layer else input_dim
+
+        # Pass in kernels, specify output dim (shared hyperparams/variables)
+
+        inducing_var = construct_basic_inducing_variables(
+            num_inducing=model_config.num_inducing,
+            input_dim=D_in,
+            share_variables=True,
+            z_init=centroids,
+        )
+
+        kernel = construct_basic_kernel(
+            kernels=build_kernel(D_in, is_last_layer, model_config.kernel),
+            output_dim=D_out,
+            share_hyperparams=True,
+        )
+
+        assert model_config.whiten is True, "non-whitened case not implemented yet"
+
+        if is_last_layer:
+            mean_function = Zero()
+            q_sqrt_scaling = 1.0
+        else:
+            mean_function = construct_mean_function(X_running, D_out)
+            X_running = mean_function(X_running)
+            if tf.is_tensor(X_running):
+                X_running = cast(tf.Tensor, X_running).numpy()
+            q_sqrt_scaling = model_config.inner_layer_qsqrt_factor
+
+        layer = GPLayer(
+            kernel,
+            inducing_var,
+            num_data,
+            mean_function=mean_function,
+            name=f"gp_{i_layer}",
+        )
+        layer.q_sqrt.assign(layer.q_sqrt * q_sqrt_scaling)
+        gp_layers.append(layer)
+
+    likelihood = model_config.likelihood.create()
+
+    return DeepGP(gp_layers, LikelihoodLayer(likelihood))
+
+
+@build_constant_input_dim_architecture.register
+def build_constant_input_dim_orthogonal_deep_gp(
+    model_config: OrthogonalModelHyperparametersConfig, X: np.ndarray
+) -> OrthDeepGP:
+    if X.dtype != default_float():
+        raise ValueError(
+            f"X needs to have dtype according to gpflow.default_float() = {default_float()} "
+            f"however got X with {X.dtype} dtype."
+        )
+
+    num_data, input_dim = X.shape
+    X_running = X
+
+    num_inducing_u = model_config.num_inducing_u
+    num_inducing_v = model_config.num_inducing_v
+    gp_layers = []
+    centroids, _ = kmeans2(X, k=min(num_inducing_u + num_inducing_v, X.shape[0]), minit="points")
+
+    centroids_u = centroids[:num_inducing_u, ...]
+    centroids_v = centroids[num_inducing_u:, ...]
+
+    num_layers = model_config.num_layers
+    for i_layer in range(num_layers):
+        is_last_layer = i_layer == num_layers - 1
+        D_in = input_dim
+        D_out = 1 if is_last_layer else input_dim
+
+        # Pass in kernels, specify output dim (shared hyperparams/variables)
+
+        inducing_var_u = construct_basic_inducing_variables(
+            num_inducing=num_inducing_u,
+            input_dim=D_in,
+            share_variables=True,
+            z_init=centroids_u,
+        )
+
+        inducing_var_v = construct_basic_inducing_variables(
+            num_inducing=num_inducing_v,
+            input_dim=D_in,
+            share_variables=True,
+            z_init=centroids_v,
+        )
+
+        kernel = construct_basic_kernel(
+            kernels=build_kernel(D_in, is_last_layer, model_config.kernel),
+            output_dim=D_out,
+            share_hyperparams=True,
+        )
+
+        assert model_config.whiten is True, "non-whitened case not implemented yet"
+
+        if is_last_layer:
+            mean_function = Zero()
+            q_sqrt_scaling = 1.0
+        else:
+            mean_function = construct_mean_function(X_running, D_out)
+            X_running = mean_function(X_running)
+            if tf.is_tensor(X_running):
+                X_running = cast(tf.Tensor, X_running).numpy()
+            q_sqrt_scaling = model_config.inner_layer_qsqrt_factor
+
+        layer = OrthGPLayer(
+            kernel,
+            inducing_var_u,
+            inducing_var_v,
+            num_data,
+            mean_function=mean_function,
+            name=f"orth_gp_{i_layer}",
+            num_latent_gps=D_out,
+        )
+        layer.q_sqrt_u.assign(layer.q_sqrt_u * q_sqrt_scaling)
+        layer.q_sqrt_v.assign(layer.q_sqrt_v * q_sqrt_scaling)
+        gp_layers.append(layer)
+
+    likelihood = model_config.likelihood.create()
+
+    return OrthDeepGP(gp_layers, LikelihoodLayer(likelihood))
